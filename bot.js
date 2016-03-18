af5dcf174b3aea775afabbbdc0c9e31e7232fcd3
Zach Case <zach@strawhat.io>
2016-03-18 13:38:45 -0600

    Start coding the bot

--- bot.js
+++ bot.js
@@ -1,73 +1,9 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+var Parse = require('parse').Parse;
+var localStorage = require('localStorage');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+Parse.initialize("7Z8Eoxjl5W0bRERvaEnnU06csww1akEUgU4WKbZ4", "Z2gHn7jyAauCfS7cWufWzoYsZ7ECiiqQIghSlKcB");
 
-
-This is a sample Slack bot built with Botkit.
-
-This bot demonstrates many of the core features of Botkit:
-
-* Connect to Slack using the real time API
-* Receive messages based on "spoken" patterns
-* Reply to messages
-* Use the conversation system to ask questions
-* Use the built in storage system to store and retrieve information
-  for a user.
-
-# RUN THE BOT:
-
-  Get a Bot token from Slack:
-
-    -> http://my.slack.com/services/new/bot
-
-  Run your bot from the command line:
-
-    token=<MY TOKEN> node bot.js
-
-# USE THE BOT:
-
-  Find your bot inside Slack to send it a direct message.
-
-  Say: "Hello"
-
-  The bot will reply "Hello!"
-
-  Say: "who are you?"
-
-  The bot will tell you its name, where it running, and for how long.
-
-  Say: "Call me <nickname>"
-
-  Tell the bot your nickname. Now you are friends.
-
-  Say: "who am I?"
-
-  The bot will tell you your nickname, if it knows one for you.
-
-  Say: "shutdown"
-
-  The bot will ask if you are sure, and then shut itself down.
-
-  Make sure to invite your bot into other channels using /invite @<my bot>!
-
-# EXTEND THE BOT:
-
-  Botkit is has many features for building cool and useful bots!
-
-  Read all about it here:
-
-    -> http://howdy.ai/botkit
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-
-if (!process.env.token) {
-    console.log('Error: Specify token in environment');
-    process.exit(1);
-}
+var botToken = 'xoxb-27826312929-yALOa37k1d6zGHdB1oCNOCII';
 
 var Botkit = require('./lib/Botkit.js');
 var os = require('os');
@@ -77,9 +13,30 @@ var controller = Botkit.slackbot({
 });
 
 var bot = controller.spawn({
-    token: process.env.token
+    token: botToken
 }).startRTM();
 
+// START OF CUSTOM LISTENERS
+
+controller.hears(['list players'], 'direct_message,direct_mention,mention', function(bot,message) {
+  var Players = Parse.Object.extend("Players");
+  var query = new Parse.Query(Players);
+  query.find({
+    success: function(results) {
+      var response = "Here are the list of users I have saved:";
+          response = (results.length <= 0 ? response + " No Saved Players" : response );
+
+      for(var i = 0; i < results.length; i++) {
+        var player = object[i];
+        response = response + " " + player.get('name');
+      }
+
+      bot.reply(message, response);
+    }
+  });
+});
+
+// START OF EXAMPLE LISTENERS - LEAVING IN FOR REFERENCE SAKE, WILL REMOVE WHEN READY or COMMENT OUT
 
 controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot, message) {
 
